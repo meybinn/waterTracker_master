0a9428d6784971b7a6c9ca1c92b9169a33f38014
Farihah <rihafarihah7@gmail.com>
2025-02-17 21:20:29 +0900

    Update history_screen
    진짜 업데이트

--- water_tracker/lib/features/history_screen.dart
+++ water_tracker/lib/features/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:water_tracker/constant/gaps.dart';
 import 'package:water_tracker/constant/sizes.dart';
@@ -13,14 +14,32 @@ class HistoryScreen extends StatefulWidget {
 class _HistoryScreenState extends State<HistoryScreen> {
   DateTime _selectedDate = DateTime.now();
 
-  final Map<DateTime, List<String>> _intakeWater = {
-    DateTime(2025, 2, 15): ['1500 ml'],
-    DateTime(2025, 2, 16): ['3000 ml'],
-  };
+  final Map<DateTime, List<String>> _intakeWater = {};
+
+  void _updateDate(DateTime newDate) {
+    setState(() {
+      _selectedDate = newDate;
+
+      bool dataExist = _intakeWater.keys.any((date) =>
+          date.year == newDate.year &&
+          date.month == newDate.month &&
+          date.day == newDate.day);
+
+      if (!dataExist) {
+        _intakeWater[newDate] = ['0 ml'];
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<String> dailyRecords = _intakeWater[_selectedDate] ?? [];
+    List<String> dailyRecords = _intakeWater.entries
+        .where((entry) =>
+            entry.key.year == _selectedDate.year &&
+            entry.key.month == _selectedDate.month &&
+            entry.key.day == _selectedDate.day)
+        .map((entry) => entry.value.join('. '))
+        .toList();
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
@@ -51,34 +70,52 @@ class _HistoryScreenState extends State<HistoryScreen> {
                   initialDate: _selectedDate,
                   firstDate: DateTime(2025),
                   lastDate: DateTime(2030),
-                  onDateChanged: (newDate) {
-                    setState(() {
-                      _selectedDate = newDate;
-                    });
-                  },
+                  onDateChanged: _updateDate,
                 ),
               ),
             ),
             Gaps.v20,
-            dailyRecords.isEmpty
-                ? Text(
-                    "No records for this day",
-                    style: GoogleFonts.righteous(
-                      color: Color(0XFF7C7C7C),
-                      fontSize: Sizes.size20,
+            Expanded(
+              child: ListView.separated(
+                itemCount: _intakeWater.length,
+                separatorBuilder: (context, index) => Divider(),
+                itemBuilder: (context, index) {
+                  DateTime date = _intakeWater.keys.elementAt(index);
+                  String formatDate = "${date.month}/${date.day}";
+                  return ListTile(
+                    leading: Padding(
+                      padding: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      child: FaIcon(
+                        FontAwesomeIcons.glassWater,
+                        color: Color(0XFF7C7C7C),
+                      ),
                     ),
-                  )
-                : Expanded(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: dailyRecords.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(dailyRecords[index]),
-                        );
-                      },
+                    title: Padding(
+                      padding: EdgeInsets.only(
+                        left: 40,
+                      ),
+                      child: Text(
+                        formatDate,
+                        style: GoogleFonts.righteous(
+                            color: Color(0XFF7C7C7C), fontSize: Sizes.size20),
+                      ),
                     ),
-                  ),
+                    trailing: Padding(
+                      padding: EdgeInsets.only(
+                        right: 40,
+                      ),
+                      child: Text(
+                        _intakeWater[date]!.join('. '),
+                        style: GoogleFonts.righteous(
+                            color: Color(0XFF7C7C7C), fontSize: Sizes.size20),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
